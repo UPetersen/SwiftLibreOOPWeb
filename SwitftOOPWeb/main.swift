533a5b3e9f02c495b95fd09ca59cfda021e97a2f
dabear <bjorninge@gmail.com>
2018-04-08 11:41:49 +0200

    Initial Commit

---
+++ SwitftOOPWeb/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwitftOOPWeb
+//
+//  Created by Bjørn Inge Berg on 08.04.2018.
+//  Copyright © 2018 Bjørn Inge Berg. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
